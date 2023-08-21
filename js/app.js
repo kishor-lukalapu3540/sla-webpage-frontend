290ab0966a403a081440720c9c219e9638498296
kishor <kishorlukalapu143@gmail.com>
2023-08-21 10:45:18 +0530

    sla website project completed

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// DOM - Document Object Model
+
+// console.log(window);
+// console.log(window.document);
+// console.log(document);
+
+const navContainer = document.querySelector(".nav-container");
+console.log(navContainer);
+
+const pageContainer = document.querySelector(".page-hero-container"
+);
+console.log(pageContainer);
+
